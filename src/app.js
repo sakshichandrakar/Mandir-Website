8790a8d2efdf1393e3db09232a439681ccf802fc
sakshichandrakar <sakshichandrakar2911@gmail.com>
2023-12-30 19:57:01 +0530

    Working on some pages

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const hbs = require("hbs");
 const bodyParser = require('body-parser');
 const webDetailController = require('./controllers/webDetailController');
 const loginController = require('./controllers/loginController');
+const testimonialController = require('./controllers/testimonialController');
 
 const app = express();
 require("./db/conn");
@@ -26,8 +27,7 @@ app.use(express.static('public'));
 const fetchData = async () => {
     try {
       const webDetail = await webDetailController.getWebDetailsWithoutExpress();
-      return webDetail; // Return the fetched data
-
+      return webDetail;
     } catch (error) {
       console.error(error);
     }
@@ -36,17 +36,17 @@ const fetchData = async () => {
   app.get("", async (req, res) => {
     try {
       let webDetail = await fetchData(); 
-      res.render("FrontEnd/index", { title: "Home", webDetail});
+      const testimonial_data = await testimonialController.gettestimonial();
+      res.render("FrontEnd/index", { title: "Home", webDetail,testimonial_data});
     } catch (error) {
-      res.status(500).send('Internal Server Error');
-    }
+      res.render("FrontEnd/404", { title: "Error Page",webDetail, message: "Internal Server Error"});    }
   });
  app.get("/about", async (req,res) =>{
     try {
     let webDetail = await fetchData();
     res.render("FrontEnd/about", { title: "About", webDetail});
   } catch (error) {
-    res.status(500).send('Internal Server Error');
+    res.render("FrontEnd/404", { title: "Error Page",webDetail, message: "Internal Server Error"});  
   }
  });
 app.get("/contact", async (req,res) =>{
@@ -54,8 +54,7 @@ app.get("/contact", async (req,res) =>{
         let webDetail = await fetchData(); 
         res.render("FrontEnd/contact", { title: "Contact", webDetail});
       } catch (error) {
-        res.status(500).send('Internal Server Error');
-      }
+  res.render("FrontEnd/404", { title: "Error Page",webDetail, message: "Internal Server Error"});      }
 });
 
 app.get("/service",async (req,res) =>{
@@ -63,8 +62,7 @@ app.get("/service",async (req,res) =>{
         let webDetail = await fetchData(); 
         res.render("FrontEnd/service", { title: "Service", webDetail});
       } catch (error) {
-        res.status(500).send('Internal Server Error');
-      }
+      res.render("FrontEnd/404", { title: "Error Page",webDetail, message: "Internal Server Error"});      }
 });
 
 app.get("/blog",async (req,res) =>{
@@ -72,8 +70,7 @@ app.get("/blog",async (req,res) =>{
         let webDetail = await fetchData(); 
         res.render("FrontEnd/blog", { title: "Blog", webDetail});
       } catch (error) {
-        res.status(500).send('Internal Server Error');
-      }
+      res.render("FrontEnd/404", { title: "Error Page",webDetail, message: "Internal Server Error"});      }
 });
 
 app.get("/blog-details", async(req,res) =>{
@@ -81,8 +78,7 @@ app.get("/blog-details", async(req,res) =>{
         let webDetail = await fetchData(); 
         res.render("FrontEnd/blog-details", { title: "Blog Detail", webDetail});
       } catch (error) {
-        res.status(500).send('Internal Server Error');
-      }
+      res.render("FrontEnd/404", { title: "Error Page",webDetail, message: "Internal Server Error"});      }
 });
 
 app.get("/broadcast", async(req,res) =>{
@@ -90,7 +86,7 @@ app.get("/broadcast", async(req,res) =>{
         let webDetail = await fetchData(); 
         res.render("FrontEnd/broadcast", { title: "Broadcast", webDetail});
         } catch (error) {
-        res.status(500).send('Internal Server Error');
+          res.render("FrontEnd/404", { title: "Error Page",webDetail, message: "Internal Server Error"});  
         }
 });
 app.get("/faq", async(req,res) =>{
@@ -98,7 +94,7 @@ app.get("/faq", async(req,res) =>{
         let webDetail = await fetchData(); 
         res.render("FrontEnd/faq", { title: "Faq", webDetail});
         } catch (error) {
-        res.status(500).send('Internal Server Error');
+          res.render("FrontEnd/404", { title: "Error Page",webDetail, message: "Internal Server Error"});        
         }
 });
 app.get("/volunteers", async(req,res) =>{
@@ -106,7 +102,7 @@ app.get("/volunteers", async(req,res) =>{
         let webDetail = await fetchData(); 
         res.render("FrontEnd/volunteers", { title: "Valunteers", webDetail});
         } catch (error) {
-        res.status(500).send('Internal Server Error');
+          res.render("FrontEnd/404", { title: "Error Page",webDetail, message: "Internal Server Error"}); 
         }
 });
 app.get("/volunteer-detail",async (req,res) =>{
@@ -114,15 +110,15 @@ app.get("/volunteer-detail",async (req,res) =>{
         let webDetail = await fetchData(); 
         res.render("FrontEnd/volunteer-detail", { title: "Valunteer Details", webDetail});
         } catch (error) {
-        res.status(500).send('Internal Server Error');
-        }
+          res.render("FrontEnd/404", { title: "Error Page",webDetail, message: "Internal Server Error"});   
+       }
 });
 app.get("/events", async(req,res) =>{
     try {
         let webDetail = await fetchData(); 
         res.render("FrontEnd/events", { title: "Event", webDetail});
         } catch (error) {
-        res.status(500).send('Internal Server Error');
+        res.render("FrontEnd/404", { title: "Error Page",webDetail, message: "Internal Server Error"});  
         }
 });
 app.get("/puja",async (req,res) =>{
@@ -130,15 +126,15 @@ app.get("/puja",async (req,res) =>{
         let webDetail = await fetchData(); 
         res.render("FrontEnd/puja", { title: "Puja", webDetail});
         } catch (error) {
-        res.status(500).send('Internal Server Error');
-        }
+        res.render("FrontEnd/404", { title: "Error Page",webDetail, message: "Internal Server Error"}); 
+       }
 });
 app.get("/puja-details",async (req,res) =>{
     try {
         let webDetail = await fetchData(); 
         res.render("FrontEnd/puja-details", { title: "Puja Details", webDetail});
         } catch (error) {
-        res.status(500).send('Internal Server Error');
+         res.render("FrontEnd/404", { title: "Error Page",webDetail, message: "Internal Server Error"});    
         }
     res.render("FrontEnd/puja-details");
 });
@@ -147,61 +143,78 @@ app.get("/holi",async (req,res) =>{
         let webDetail = await fetchData(); 
         res.render("FrontEnd/holi", { title: "Holi", webDetail});
         } catch (error) {
-        res.status(500).send('Internal Server Error');
-        }
-    res.render("FrontEnd/holi");
+        res.render("FrontEnd/404", { title: "Error Page",webDetail, message: "Internal Server Error"});  
+       }
 
 });app.get("/volunteer-detail",async (req,res) =>{
 try {
     let webDetail = await fetchData(); 
     res.render("FrontEnd/volunteer-detail", { title: "Volunteer Detail", webDetail});
     } catch (error) {
-    res.status(500).send('Internal Server Error');
-    }
+    res.render("FrontEnd/404", { title: "Error Page",webDetail, message: "Internal Server Error"});   
+   }
 });
 app.get("/holi-details",async (req,res) =>{
     try {
         let webDetail = await fetchData(); 
         res.render("FrontEnd/holi-details", { title: "Holi Detail", webDetail});
         } catch (error) {
-        res.status(500).send('Internal Server Error');
-        }
+        res.render("FrontEnd/404", { title: "Error Page",webDetail, message: "Internal Server Error"}); 
+       }
 });
 
 app.get("/admin-login", (req,res) =>{
-    res.render("BackEnd/login");
+    res.render("BackEnd/login",{ title: "Admin Login"});
 });
 
 app.get("/blank", (req,res) =>{
-    res.render("BackEnd/blank");
+    res.render("BackEnd/blank",{ title: "Blank"});
 });
 
 app.get("/forgot-password", (req,res) =>{
-    res.render("BackEnd/forgot-password");
+    res.render("BackEnd/forgot-password",{ title: "Forgot Password"});
 });
 
 app.get("/dashboard", (req,res) =>{
-    res.render("BackEnd/dashboard");
+    res.render("BackEnd/dashboard",{ title: "Dashboard"});
 });
 
 app.get("/add-volunteers", (req,res) =>{
-    res.render("BackEnd/add-volunteers");
+    res.render("BackEnd/add-volunteers",{ title: "Add Volunteers"});
 });
 
 app.get("/web-details", (req,res) =>{
-    res.render("BackEnd/web-detail");
+    res.render("BackEnd/web-detail",{ title: "Web Details"});
 });
 
+app.get("/testimonial", async (req,res) =>{
+  const testimonial_data = await testimonialController.gettestimonial();
+  res.render("BackEnd/testimonials",{testimonial_data, title: "Testmonials"},);
+});
+
+app.get("/add-testimonial", async (req,res) =>{
+  const testimonial_data = await testimonialController.gettestimonial();
+  res.render("BackEnd/add_testimonials",{testimonial_data, title: "Add Testimonial"});
+});
+
+app.get('/testimonialedit', async (req, res) => {
+  const testimonialId = req.query.id;
+  const testimonial_data = await testimonialController.gettestimonialId(testimonialId);
+  try {
+     res.render("BackEnd/add_testimonials",{testimonial_data, title: "Add Testimonial"});
+  } catch (error) {
+    res.render("FrontEnd/404", { title: "Error Page",webDetail, message: "Internal Server Error"});  }
+});
 
 app.post('/web-details', webDetailController.createWebDetail);
 app.get("/web-details-data", webDetailController.getWebDetails);
-
 app.post("/admin-login", loginController.login);
+app.post('/testimonial', testimonialController.Addtestimonial);
+app.get("/testimonial-list", testimonialController.gettestimonial);
 
-app.get("*", (req,res) =>{
-    res.render("FrontEnd/404",{
-        errormsg : "Opps! Page Not Found",
-      });
+app.get("*", async(req,res) =>{
+  let webDetail = await fetchData(); 
+  res.render("FrontEnd/404", { title: "Error Page",webDetail, message: "Opps! Page Not Found"});  
 });
 
 app.listen(port ,() =>{
